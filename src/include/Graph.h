cb1c3827597da8ec532d5a68dea196daa472a2e1
pr0kuk <alex-shch@yandex.ru>
2023-04-06 20:36:09 +0300

    fixed registers

--- src/include/Graph.h
+++ src/include/Graph.h
@@ -4,10 +4,10 @@
 #include <vector>
 using vector2d = std::vector<std::vector<unsigned>>;
 class Graph {
-  vector2d Vertices;
-  vector2d Edges;
+  vector2d vertices;
+  vector2d edges;
 public:
   Graph(std::ifstream &);
-  vector2d &get_vertices() { return Vertices; }
-  vector2d &get_edges() { return Edges; }
+  vector2d &get_vertices() { return vertices; }
+  vector2d &get_edges() { return edges; }
 };
--- src/lib/Graph.cpp
+++ src/lib/Graph.cpp
@@ -6,13 +6,13 @@ Graph::Graph(std::ifstream &input) {
   unsigned num_edges = 0, num_vertices = 0, c_vert = 0;
   std::string line;
   input >> num_edges >> num_vertices;
-  Vertices = vector2d(num_vertices + 1);
-  Edges = vector2d(num_edges + 1);
+  vertices = vector2d(num_vertices + 1);
+  edges = vector2d(num_edges + 1);
   for (unsigned c_edge = 0; getline(input, line); c_edge++){
-    std::istringstream Stream(line);
-    while (Stream >> c_vert) {
-      Vertices[c_vert].push_back(c_edge);
-      Edges[c_edge].push_back(c_vert);
+    std::istringstream stream(line);
+    while (stream >> c_vert) {
+      vertices[c_vert].push_back(c_edge);
+      edges[c_edge].push_back(c_vert);
     }
   }
 }
