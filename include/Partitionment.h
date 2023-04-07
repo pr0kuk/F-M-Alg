a63b05eaccd717f6f4d009d4fd7a90617acb4964
pr0kuk <shcherbakov.aa@phystech.edu>
2023-04-08 01:52:13 +0300

    deleted extra include

--- include/Partitionment.h
+++ include/Partitionment.h
@@ -1,19 +1,18 @@
 #pragma once
 #include "Graph.h"
-#include <vector>
 
 class Partitionment {
-  std::vector<int> vertices;
-  int cost = 0;
-  int color = 0;
-  Graph &graph;
-public:
-  Partitionment(Graph graph);
-  void set_cost(int cost) { this->cost = cost; }
-  int get_cost() { return cost; }
-  int get_color() { return color; }
-  void calculate_cost();
-  void apply(unsigned move);
-  void print(std::ostream &Out = std::cout);
-  std::vector<int> &get_prt() { return vertices; }
+        std::vector<int> vertices;
+        int cost = 0;
+        int color = 0;
+        Graph &graph;
+    public:
+        Partitionment(Graph graph);
+        void set_cost(int cost) { this->cost = cost; }
+        int get_cost() { return cost; }
+        int get_color() { return color; }
+        void calculate_cost();
+        void apply(unsigned move);
+        void print(std::ostream &Out = std::cout);
+        std::vector<int> &get_prt() { return vertices; }
 };
