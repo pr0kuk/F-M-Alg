100afd818b4ffc64243232742e02b35f2c290808
pr0kuk <shcherbakov.aa@phystech.edu>
2023-04-08 01:55:43 +0300

    fixed idents

--- include/Graph.h
+++ include/Graph.h
@@ -4,10 +4,10 @@
 #include <vector>
 using vector2d = std::vector<std::vector<unsigned>>;
 class Graph {
-  vector2d vertices;
-  vector2d edges;
+    vector2d vertices;
+    vector2d edges;
 public:
-  Graph(std::ifstream &);
-  vector2d &get_vertices() { return vertices; }
-  vector2d &get_edges() { return edges; }
+Graph(std::ifstream &);
+    vector2d &get_vertices() { return vertices; }
+    vector2d &get_edges()    { return edges; }
 };
