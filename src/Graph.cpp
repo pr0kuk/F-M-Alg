3b5de7c41f87990573239d731ae82bc88cb4b83b
pr0kuk <shcherbakov.aa@phystech.edu>
2023-04-08 01:55:18 +0300

    simplified names

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -3,16 +3,16 @@
 #include <string>
 
 Graph::Graph(std::ifstream &input) {
-  unsigned num_edges = 0, num_vertices = 0, c_vert = 0;
-  std::string line;
-  input >> num_edges >> num_vertices;
-  vertices = vector2d(num_vertices + 1);
-  edges = vector2d(num_edges + 1);
-  for (unsigned c_edge = 0; getline(input, line); c_edge++){
-    std::istringstream stream(line);
-    while (stream >> c_vert) {
-      vertices[c_vert].push_back(c_edge);
-      edges[c_edge].push_back(c_vert);
+    unsigned num_edges = 0, num_vertices = 0, v = 0;
+    std::string line;
+    input >> num_edges >> num_vertices;
+    vertices = vector2d(num_vertices + 1);
+    edges = vector2d(num_edges + 1);
+    for (unsigned e = 0; getline(input, line); e++){
+        std::istringstream stream(line);
+        while (stream >> v) {
+            vertices[v].push_back(e);
+            edges[e].push_back(v);
+        }
     }
-  }
 }
