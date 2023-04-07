a8dce0e27f5ac1676dfab09492b6477e603a1c46
pr0kuk <shcherbakov.aa@phystech.edu>
2023-04-08 00:42:19 +0300

    fixed output name

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,15 @@
 #include <chrono>
 unsigned FM(Graph & graph, Partitionment & prt);
 int main(int argc, char *argv[]) {
-  std::string input_file = argv[1];
-  std::ifstream input(input_file);
-  std::ofstream output(input_file + ".part.1.txt");
-  Graph graph(input);
-  Partitionment prt(graph);
-  auto start = std::chrono::steady_clock::now();
-  unsigned iterations = FM(graph, prt);
-  auto time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count();
-  //OutFile << input_file << ' ' << Graph.getEdges().size() << ' ' << Graph.getVertices().size() << ' ' << Prt.getCost() << ' ' << time << ' ' << iterations << '\n';
-  prt.print(output);
-  return 0;
+    std::string input_file = argv[1];
+    std::ifstream input(input_file);
+    std::ofstream output(input_file + ".part.2.txt");
+    Graph graph(input);
+    Partitionment prt(graph);
+    auto start = std::chrono::steady_clock::now();
+    unsigned iterations = FM(graph, prt);
+    auto time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count();
+    std::cout << input_file << ' ' << graph.get_edges().size() << ' ' << graph.get_vertices().size() << ' ' << prt.get_cost() << ' ' << time << ' ' << iterations << '\n';
+    prt.print(output);
+    return 0;
 }
