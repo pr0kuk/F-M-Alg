19c9459467395986e0bdd4ab512f6cebd6f9a9dc
pr0kuk <shcherbakov.aa@phystech.edu>
2023-04-08 01:24:20 +0300

    replaced set->list

--- include/GainContainer.h
+++ include/GainContainer.h
@@ -2,23 +2,25 @@
 #include <iostream>
 #include <map>
 #include <set>
+#include <list>
 #include <vector>
-using Colors = std::map<int, std::set<int>>;
+using Colors = std::map<int, std::list<int>>;
+// using Colors = std::map<int, std::set<int>>;
 using Move = std::pair<unsigned, int>;
 class Graph;
 class Partitionment;
 class GainContainer {
-public:
-  GainContainer(Graph&, Partitionment&);
-  Move best_feasible_move(int color);
-  bool is_empty(int color);
-  void update(unsigned vertex, int color, int value);
-  void erase(unsigned vertex, int color);
-  void update_deleted(unsigned vertex);
-private:
-  Colors first;
-  Colors second;
-  std::vector<int> gain;
-  std::set<unsigned> deleted;
-  Colors &get_needed_color(int color);
+    public:
+        GainContainer(Graph&, Partitionment&);
+        Move best_feasible_move(int color);
+        bool is_empty(int color);
+        void update(unsigned vertex, int color, int value);
+        void erase(unsigned vertex, int color);
+        void update_deleted(unsigned vertex);
+    private:
+        Colors first;
+        Colors second;
+        std::vector<int> gain;
+        std::set<unsigned> deleted;
+        Colors &get_needed_color(int color);
 };
--- src/GainContainer.cpp
+++ src/GainContainer.cpp
@@ -1,60 +1,65 @@
 #include "GainContainer.h"
 #include "Graph.h"
 #include "Partitionment.h"
-
+#include <algorithm>
 GainContainer::GainContainer(Graph &graph, Partitionment &prt) {
-  size_t prt_size = prt.get_prt().size();
-  gain.resize(prt_size);
-  for (unsigned i = 0; i != prt_size; ++i) {
-    int current_gain = 0, current_prt = prt.get_prt()[i];
-    for (unsigned e : graph.get_vertices()[i]) {
-      bool from_block = true, to_block = true;
-      for (unsigned v : graph.get_edges()[e]) {
-        if (current_prt != prt.get_prt()[v])
-          to_block = false;
-        else if (v != i)
-          from_block = false;
-      }
-      if (from_block)
-        current_gain++;
-      if (to_block)
-        current_gain--;
+    size_t prt_size = prt.get_prt().size();
+    gain.resize(prt_size);
+    for (unsigned i = 0; i != prt_size; ++i) {
+        int current_gain = 0, current_prt = prt.get_prt()[i];
+        for (unsigned e : graph.get_vertices()[i]) {
+            bool from_block = true, to_block = true;
+        for (unsigned v : graph.get_edges()[e]) {
+            if (current_prt != prt.get_prt()[v])
+                to_block = false;
+            else if (v != i)
+                from_block = false;
+        }
+        if (from_block)
+            current_gain++;
+        if (to_block)
+            current_gain--;
+        }
+        get_needed_color(current_prt)[current_gain].push_back(i);
+        // get_needed_color(current_prt)[current_gain].insert(i);
+        gain[i] = current_gain;
     }
-    get_needed_color(current_prt)[current_gain].insert(i);
-    gain[i] = current_gain;
-  }
 }
 
 Colors &GainContainer::get_needed_color(int color) {
-  return color ? second : first;
+    return color ? second : first;
 }
 
 bool GainContainer::is_empty(int color)  {
-  return get_needed_color(color).empty();
+    return get_needed_color(color).empty();
 }
 
 Move GainContainer::best_feasible_move(int color) {
-  Colors &c = get_needed_color(color);
-  auto &&[gain, vertices] = *c.rbegin();
-  unsigned v = *vertices.begin();
-  vertices.erase(v);
-  if (vertices.empty())
-    c.erase(gain);
-  return std::make_pair(v, gain);
+    Colors &c = get_needed_color(color);
+    auto &&[gain, vertices] = *c.rbegin();
+    unsigned v = vertices.front();
+    vertices.pop_front();
+    // unsigned v = *vertices.begin();
+    // vertices.erase(v);
+    if (vertices.empty())
+        c.erase(gain);
+    return std::make_pair(v, gain);
 }
 
 void GainContainer::update(unsigned v, int color, int value) {
-  if (deleted.count(v))
-    return;
-  erase(v, color);
-  gain[v] += value;
-  get_needed_color(color)[gain[v]].insert(v);
+    if (deleted.count(v))
+        return;
+    erase(v, color);
+    gain[v] += value;
+    get_needed_color(color)[gain[v]].push_front(v);
+    // get_needed_color(color)[gain[v]].insert(v);
 }
 
 void GainContainer::erase(unsigned v, int color) {
-  get_needed_color(color)[gain[v]].erase(v);
-  if (get_needed_color(color)[gain[v]].empty())
-    get_needed_color(color).erase(gain[v]);
+    get_needed_color(color)[gain[v]].remove(v);
+    // get_needed_color(color)[gain[v]].erase(v);
+    if (get_needed_color(color)[gain[v]].empty())
+        get_needed_color(color).erase(gain[v]);
 }
 
 void GainContainer::update_deleted(unsigned v) { deleted.insert(v); }
