4271d3eb5bd6883275acdb695e1489bd65315e05
pr0kuk <alex-shch@yandex.ru>
2023-04-09 01:30:20 +0300

    fixed .py script

--- scr.py
+++ scr.py
@@ -1,21 +1,16 @@
 from pathlib import Path
-import os
 import subprocess
-import threading as th
  
 directory = 'benchmark_set'
-files = Path(directory).glob('ISPD*.hgr')
-print(files)
+files = Path(directory).glob('*.hgr')
 
-def doProcessing(flag):
+def run_flag():
     for file in files:
-        process_one = subprocess.run(['build/Debug/fm.exe', flag, file])
+        process_one = subprocess.run(['build/Debug/fm.exe', '-m', file])
 
-def doProcessing():
+def run():
     for file in files:
         process_one = subprocess.run(['build/Debug/fm.exe', file])
 
-
-
-doProcessing()
-doProcessing('-m')
+run()
+run_flag()
